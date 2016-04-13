0edc7d8b31e8f3297445ace26f509e89d46e1abf
s14007 <s14007@std.it-college.ac.jp>
2016-04-13 15:24:41 +0900

    commit

--- 4.Computation/D.java
+++ 4.Computation/D.java
@@ -1,28 +1,19 @@
 import java.util.*;
 
-//見た目一緒
 public class D {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        int[] a = new int[n];
-        int max, min, sum = 0;
+        long max, min, sum = 0;
+        ArrayList<Integer> z = new ArrayList<>();
 
-        for (int i = 0; i < a.length; i++) {
-            a[i] = scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            z.add(scan.nextInt());
+            sum += z.get(i);
         }
+        max = Collections.max(z);
+        min = Collections.min(z);
 
-        max = a[0];
-        min = a[0];
-
-        for (int num : a) {
-            if (max < num) {
-                max = num;
-            } else if (min > num) {
-                min = num;
-            }
-            sum += num;
-        }
-        System.out.printf("%d %d %d", min, max, sum);
+        System.out.printf(min + " " + max + " " + sum);
     }
 }
